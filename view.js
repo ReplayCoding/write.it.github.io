dcceee41ccd24194a749d43861e66c0dbf2d07d6
Replay <saeedcamposijaz@gmail.com>
2017-08-09 12:56:29 -0400

    Finished write.it
    Yay!

--- view.js
+++ view.js
@@ -1,4 +1,8 @@
 $(function(){
-	var db = firebase.database().ref();
-	
-})
+	var content;
+	var db = firebase.database().ref(location.search.slice(1,location.search.length));
+	db.once('value').then(function(snapshot) {
+  	var content = snapshot.val();
+  	$("#content").text(content);
+  });
+});
